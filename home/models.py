6da25618d15a98f3081d6abb90cea3f63c14f951
sadriddinoov <bilolsadriddinov941@gmail.com>
2025-07-28 12:35:58 +0500

    Added header get for header and hero including bg

--- home/models.py
+++ home/models.py
@@ -8,5 +8,7 @@ class Title(BaseModel):
     def __str__(self):
         return f'{self.name}'
 
-
-
+class Header(BaseModel):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    bg_image = models.ImageField(upload_to="header")
---
+++ home/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Header
+
+class HeaderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Header
+        fields = '__all__'
